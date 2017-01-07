312c495d9291220b91744821d43239e9f891c426
zlsh80826 <zlsh80826@gmail.com>
2017-01-07 20:25:04 +0800

    init select from two signals mux

---
+++ two_to_one_mux.v
@@ -0,0 +1,18 @@
+module two_to_one_mux #(
+  parameter BITS  
+) (
+  input select;
+  input [BITS - 1 : 0] in0;
+  input [BITS - 1 : 0] in1;
+  output reg [BITS - 1 : 0] out
+);
+
+  always@(*) begin
+    if ( select == 1'b1 ) begin
+      out = in1;
+    end else begin
+      out = in0;
+    end
+  end
+
+endmodule
